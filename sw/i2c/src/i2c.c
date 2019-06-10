57d24026288fa1dd7521a17dc3526caa496d268f
mtnguyen01 <minhtoan.nguyen@edu.esiee.fr>
2019-06-10 17:34:52 +0200

    update 6/10/2019

--- sw/i2c/src/i2c.c
+++ sw/i2c/src/i2c.c
@@ -4,15 +4,17 @@
 #include <string.h>
 #include <stdint.h>
 
-#define SLAVE_ADDR 0x55 //Slave Addres
+#define SLAVE_ADDR 0x55 //Slave Addres + '0' --> 1010101 + '0'
+#define MEMA 0xF8
 #define BAUDERATE 100000 //100kHz
 #define RAM_REG 
 
+char *bt_addr(char *buf[]);
 
 int main(void){
 	char buf[1];
 	char buf1[1];
-	char buf2[10];
+	char buf2[16];
 	int i;
 	
 	if(!bcm2835_init()){
@@ -23,7 +25,7 @@ int main(void){
 	
 	bcm2835_i2c_begin();
 	bcm2835_i2c_setSlaveAddress(SLAVE_ADDR);
-	buf[0] = 0x01; //block address with 16 bytes offset
+	buf[0] = 0x04;
 	int ack = bcm2835_i2c_write(buf,1);
 	printf("ACK : %d\n", ack);
 	bcm2835_i2c_end();
@@ -39,24 +41,24 @@ int main(void){
 		printf("Read Buf[%d] = %x\n", i, buf2[i]);
 	}
 	bcm2835_i2c_end();
-	
 
+	char *addr = bt_addr(buf2);
 
-// 	while(1){
-// /*		bcm2835_i2c_begin();
-// 		bcm2835_i2c_setSlaveAddress(SLAVE_ADDR1);*/
-// 		buf[0] = 0xF8;
-// 		int ack = bcm2835_i2c_write(buf,1);
-// 		printf("ACK : %d\n", ack);
-// 		bcm2835_delayMicroseconds(10000);
-// 		bcm2835_i2c_write(buf1,1);
-		
+	printf("%s\n", );
 
-// 		bcm2835_delayMicroseconds(60);
-// 		bcm2835_i2c_begin();
-// 		bcm2835_i2c_setSlaveAddress(SLAVE_ADDR2);
-// 		bcm2835_i2c_read(buf2,15);
-// 		bcm2835_i2c_stop();*
-// 	}
 
 }	
+
+
+char *bt_addr(char *buf[])
+{
+	int i = 0;
+	char *bt_addr[128];
+
+	for(i = sizeof(buf); i<= 0 ; i--)
+	{
+		printf("Read Buf2[%d] = %x\n", i, buf[i]);
+
+	}
+	return bt_addr;
+}
